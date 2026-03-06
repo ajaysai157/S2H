b6804fb06585413590cdf854a9d2955b91b90ca6
Ajaysai157 <bhargavchonga976@gmail.com>
2026-03-06 19:42:39 +0530

    Basic problems on bit manipulation

---
+++ Bit Manipulation/BitManipulation2.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class BitManipulation2 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int num=sc.nextInt();
+        int count=0;
+        while(num>0){
+            num=num&num-1;
+            count++;
+        }
+        System.out.println(count);
+        sc.close();
+    }
+}
